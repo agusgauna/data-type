a3834574364b73bda57f3ef3448bac3aeee1936c
Agustina <agustinagauna@gmail.com>
2020-07-23 22:11:22 -0300

    First commit of initial project

---
+++ data-type.js
@@ -0,0 +1,12 @@
+// tipo de datos
+
+var num = 2;
+console.log(typeof num);
+console.log(num);
+console.error(num);
+console.info(num);
+console.warn(num);
+
+num = 'Hola mundo';
+console.log(typeof num);
+console.log(num);
